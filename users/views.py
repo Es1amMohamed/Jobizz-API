90cc65642144fc3c0e406eec4552c97c53a75518
Eslam Mohamed <eslammohamedmetwally1@gmail.com>
2024-04-27 23:30:08 +0300

    create sign up, login and make token for users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,89 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
+from .serializers import SignUpSerializer, UserSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
+
+
+@api_view(["POST"])
+def signup(request):
+    """
+    this function is used to create a new user
+
+    """
+    data = request.data
+    user = SignUpSerializer(data=data)
+
+    if user.is_valid():
+        if not User.objects.filter(email=data["email"]).exists():
+            user = User.objects.create(
+                username=data["username"],
+                first_name=data["first_name"],
+                last_name=data["last_name"],
+                email=data["email"],
+                password=make_password(data["password"]),
+            )
+            user.save()
+            return Response(
+                {"message": "User created successfully"}, status=status.HTTP_201_CREATED
+            )
+        else:
+            return Response(
+                {"error": "Email already exists"}, status=status.HTTP_400_BAD_REQUEST
+            )
+    else:
+        return Response(user.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(["GET"])
+@permission_classes([IsAuthenticated])
+def get_user(request):
+    user = UserSerializer(request.user)
+
+    return Response(user.data)
+
+
+@api_view(["POST"])
+def login(request):
+    data = request.data
+
+    if User.objects.filter(username=data["username"]).exists():
+        user = User.objects.get(username=data["username"])
+        if user.check_password(data["password"]):
+            return Response({"message": "Login successful"}, status=status.HTTP_200_OK)
+        else:
+            return Response(
+                {"error": "Invalid password"}, status=status.HTTP_400_BAD_REQUEST
+            )
+    else:
+        return Response(
+            {"error": "User does not exist"}, status=status.HTTP_400_BAD_REQUEST
+        )
+
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def logout(request):
+    return Response({"message": "Logout successful"}, status=status.HTTP_200_OK)
+
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def change_password(request):
+    user = User.objects.get(id=request.user.id)
+
+    if user.check_password(request.data["password"]):
+        user.password = make_password(request.data["new_password"])
+        user.save()
+        return Response(
+            {"message": "Password changed successfully"}, status=status.HTTP_200_OK
+        )
+    else:
+        return Response(
+            {"error": "Invalid old password"}, status=status.HTTP_400_BAD_REQUEST
+        )
