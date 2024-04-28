b97580aba94e18c2f4081019c924d842f0cd36f6
Eslam Mohamed <eslammohamedmetwally1@gmail.com>
2024-04-28 22:32:21 +0300

    start in users profiles

---
+++ job/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+Job_type = (
+    ('Full Time', 'Full Time'),
+    ('Part Time', 'Part Time'),
+    ('Internship', 'Internship'),
+    ('Contract', 'Contract'),
+    ('Freelance', 'Freelance'),
+)
+
+Job_level = (
+    ('Junior', 'Junior'),
+    ('Mid Level', 'Mid Level'),
+    ('Senior Level', 'Senior Level'),
+)
+# class Job(models.Model):
+#     company_name = ''
+#     title = models.CharField(max_length=200)
+#     job_type = models.CharField(max_length=100,choices=Job_type)
+#     job_level = models.CharField(max_length=100, choices=Job_level)
+#     salary = models.IntegerField()
+#     location = models.CharField(max_length=200)
+#     description = models.TextField()
+#     requirement = models.TextField()
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
--- users/models.py
+++ users/models.py
@@ -1,4 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.utils.text import slugify
+from django.db.models.signals import post_save
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    Job_title = models.CharField(max_length=200)
+    
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+    
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)                      
